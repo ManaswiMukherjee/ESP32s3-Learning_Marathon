b9e83000a4ca80b2aa334d6a19995d2022755031
Manaswi <mukherjeemanaswi@gmail.com>
2026-08-16 01:52:37 +0530

    16/08/26 : blink and button

--- Day_1-blinky_button/main/blink_example_main.c
+++ Day_1-blinky_button/main/blink_example_main.c
@@ -4,6 +4,7 @@
 #include "driver/gpio.h"
 
 #define LED GPIO_NUM_4
+#define BUT GPIO_NUM_5
 
 void app_main(void)
 {
@@ -11,13 +12,22 @@ void app_main(void)
     gpio_reset_pin(LED);
     gpio_set_direction(LED, GPIO_MODE_OUTPUT);
 
+    //setting up button pin
+    gpio_reset_pin(BUT);
+    gpio_set_direction(BUT, GPIO_MODE_INPUT);
+    gpio_set_pull_mode(BUT, GPIO_PULLUP_ONLY);
+
     while(1)
     {
-        gpio_set_level(LED, 1);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-
-        gpio_set_level(LED, 0);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        if(gpio_get_level(BUT) == 0)
+        {
+            gpio_set_level(LED, 1);
+        }
+        else
+        {
+            gpio_set_level(LED, 0);
+        }
+        vTaskDelay(1/ portTICK_PERIOD_MS);
     }
     
 }
